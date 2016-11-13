ce41213fba67a6c08a0eb76022ab9982544ba51b
Janusz Lenkiewicz <j.lenkiewicz@ineosmart.com>
2016-11-13 13:50:03 +0100

    Wersja 1.2

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 
     $(document).ready(function() {
 
+    	// Ver. 1.2
         var paragraphs = $('.grid.grid-12');
         var anav = $(".nav a[href^='http']");
         var inputno = $('input:radio:not(:checked),input:checkbox:not(:checked)');
         var ptext =$(' #text p:first');
         var nopagin = $('.pagination-item');
 
+
         ptext.hl();
+
 
     });
 
